60cab7981b58ba6cc17b99947c2f4a584628f816
Jacob Sayles <jsayles@gmail.com>
2018-08-27 09:29:44 -0700

    added VCF address to footer

--- theme_settings.py
+++ theme_settings.py
@@ -1,6 +1,8 @@
 ALLOW_ONLINE_REGISTRATION = False
 ALLOW_PHOTO_UPLOAD = False
 
+CALENDAR_DICT = {'Public':'red', 'Member Only': 'RGBA(71, 159, 198, 1)' }
+
 FACEBOOK_URL = "https://www.facebook.com/312Main/"
 TWITTER_URL = "https://twitter.com/312main"
 #YELP_URL = ""
